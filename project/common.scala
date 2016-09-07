4fdf06f3756e774775918e303b24d556e5132fca
Yee-Ning Cheng <ycheng@tremorvideo.com>
2016-09-07 17:55:00 -0400

    Upgrade Scala to 2.11.7

--- project/common.scala
+++ project/common.scala
@@ -3,7 +3,7 @@ import sbt._
 object Common {
   import Keys._
 
-  val defaultScalaVersion = "2.11.5"
+  val defaultScalaVersion = "2.11.7"
 
   val testSettings:Seq[Setting[_]] = Seq(
     testOptions in Test += Tests.Cleanup { loader =>
